96e0bb586c69c70f6344dbe4789ca8efadddf1a3
Raju Kumar <kumarraju92501@gmail.com>
2023-10-19 22:20:46 +0530

    Demo3 has been added

---
+++ src/main/java/defaultPackage/Demo3.java
@@ -0,0 +1,12 @@
+package defaultPackage;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("huhghgtyuiifghuityuhtierhguiyiu");
+		System.out.println("huhghgtyuiifghuityuhtierhguiyiu");
+		System.out.println("huhghgtyuiifghuityuhtierhguiyiu");
+	}
+
+}
